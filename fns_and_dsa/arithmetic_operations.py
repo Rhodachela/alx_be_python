87d2a8f7231a84228cd992944f61dee3f6f75658
Rhoda <rhodachela@gmail.com>
2024-09-25 00:47:18 +0300

    Arithmetic Operation Functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,6 +10,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return("Error: Cannot divide by 0!")
+            elif num1 == 0:
+                return("Error: Cannot use o for this operation!")
             else:
                 return num1 / num2
         case _:
